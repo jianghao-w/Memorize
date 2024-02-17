993a640b8aea67d2f3edc49a7872bd4ba1a6e07b
jianghao@huskers.unl.edu <hokj123@gmail.com>
2024-02-16 22:48:22 -0600

    Create Card Views and isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,13 +7,12 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { // Behaves like a View
 
-    
     
     var body: some View {
         HStack (content:{
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
             CardView()
             CardView()
@@ -25,14 +24,21 @@ struct ContentView: View {
 
 
 struct CardView: View{
+    var isFaceUp: Bool = false
     var body: some View{
         ZStack ( content:{  // function
-            RoundedRectangle(cornerRadius: 15)
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 15)
-                .strokeBorder( lineWidth:5)
             
-            Text("🥲").font(.largeTitle)
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 15)
+                    .strokeBorder( lineWidth:5)
+                
+                Text("🥲").font(.largeTitle)
+            }else{
+                RoundedRectangle(cornerRadius: 15)
+            }
+            
             
         })
     }
