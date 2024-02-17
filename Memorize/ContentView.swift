af64dc7963d92175d3918f9e6caa61289c80e81b
jianghao@huskers.unl.edu <hokj123@gmail.com>
2024-02-16 20:10:26 -0600

    new text

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by hok on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .foregroundColor(Color.green)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Jianghao Wang!")
+            Text("How cool it is!")
+        }
+        .padding()
+    }
+}
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews:PreviewProvider {
+    static var previews: some View{
+        ContentView()
+    }
+    
+}
